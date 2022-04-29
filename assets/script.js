8c0caac89939a368ad83c5092a679eba49e8ed24
ZakyUp <andresjr195@hotmail.com>
2022-04-28 19:17:20 -0500

    onclick

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+/*   ---=== TOGGLE MENU ===--   */ 
+
+let navLinks = document.getElementById("navLinks");
+
+        function showMenu(){
+            navLinks.style.rigth = "0";
+        }
+
+        function hideMenu(){
+            navLinks.style.rigth = "-200px";
+        }
